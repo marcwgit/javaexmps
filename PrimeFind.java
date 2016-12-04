4c5826259472e1342e1ce916fbe1fceacc2ca98d
marcwgit <marcwgit@gmail.com>
2016-12-04 13:19:14 -0500

    update primefind
     update primefind to check for input and reduce iterations

--- PrimeFind.java
+++ PrimeFind.java
@@ -7,24 +7,28 @@ public static void main(String args[])throws Exception{
   
  String name="";  
 //   big problem   String primeOrNot="Prime";
+  Double testValue = 0;
  
 
 
 // start while loop and ask for input
 
-  while(!name.equals("stop")){  
-   System.out.println("Enter number: ");  
-   name=br.readLine();  
-   System.out.println("data is: "+name);  
+  while(!name.equals("stop")){
+   while (testValue < 1){
+       System.out.println("Enter number a positive integer: ");  
+       name=br.readLine();  
+       System.out.println("data is: "+name);  
  
 // declare test values and reinitialize for new test
 
-   String primeOrNot = "Prime";  
-   Double testValue = Double.valueOf(name);
+       String primeOrNot = "Prime";  
+       Double testValue = Double.valueOf(name);
+   }
 
 //  START TEST
 //  for(int i=2; i<testValue; i++){
-   for(int i=2; i<(testValue/2)+1; i++){ 
+   for(int i=2; i<(testValue/2)+1; i++){
+//   for(int i=2; i<(testValue/i)+1; i++){    // prevent tests of multiples 
  
      System.out.println(testValue + " " + i + " " + testValue%i);
      if ((testValue % i) == 0)
