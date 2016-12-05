eb2f81f137c615912e1a9f4eeaa3ff4917713e40
marcwgit <marcwgit@gmail.com>
2016-12-05 14:53:49 -0500

    Update Necklace.java

--- Necklace.java
+++ Necklace.java
@@ -27,7 +27,7 @@ public static void main(String args[])throws Exception{
    pastVal = firstNum;
    latestVal = secNum;
 
-   while ((count < 2) || (pastVal != firstNum && latestVal != secNum)){    // fails 
+ //  while ((count < 2) || (pastVal != firstNum && latestVal != secNum)){    // fails 
   while ((count < 2) || (( pastVal != firstNum) && (latestVal != secNum))){    
   //  while (count < 2 || (( pastVal != firstNum) && (latestVal != secNum))){
   // while (count < 2 || (((!(pastVal == firstNum)) && (!(latestVal == secNum))))){  //fails
