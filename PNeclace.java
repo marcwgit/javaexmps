77b0fdb73b5091e980f571dc3ddd29a74951d8e1
marcwgit <marcwgit@gmail.com>
2016-12-05 23:44:09 -0500

    Create PNeclace.java

---
+++ PNeclace.java
@@ -0,0 +1,75 @@
+import java.io.*;  
+public class PNecklace{  
+public static void main(String args[])throws Exception{  
+  
+   InputStreamReader r=new InputStreamReader(System.in);  
+   BufferedReader br=new BufferedReader(r); 
+
+
+   String num1="";
+   String num2="";
+//   int firstNum=0;
+//   int secNum=0;
+   int latestVal=0;
+   int pastVal=0;
+   int sumVal=0;
+   int count=1;
+   boolean timeToEnd = false;
+  
+ 
+   System.out.println("Enter first number: ");
+   num1=br.readLine();
+
+   System.out.println("Enter second number: ");
+   num2=br.readLine();
+ 
+//   int firstNum = Integer.valueOf(num1);
+//   int secNum = Integer.valueOf(num2);
+   int firstNum = Integer.parseInt(num1);
+   int secNum = Integer.parseInt(num2);
+   
+  System.out.print(" "+firstNum);
+  System.out.print(" "+secNum);
+   
+   pastVal = firstNum;
+   latestVal = secNum;
+
+ //  while ((count < 2) || (pastVal != firstNum && latestVal != secNum)){    // fails 
+ // while ((count < 2) || (( pastVal != firstNum) && (latestVal != secNum))){    
+  //  while (count < 2 || (( pastVal != firstNum) && (latestVal != secNum))){
+  // while (count < 2 || (((!(pastVal == firstNum)) && (!(latestVal == secNum))))){  //fails
+    while (!(pastVal == firstNum && latestVal == secNum) || count < 2){ 
+      
+     
+    
+      
+//      System.out.println("firstNum : "+firstNum+" secNum : "+secNum);
+//      System.out.println("pastVal : "+pastVal+" latestVal : "+latestVal);
+
+      sumVal = pastVal + latestVal;
+//      System.out.println(sumVal+" "+pastVal+" "+latestVal+" "+count);
+      if (sumVal > 9){
+         sumVal = sumVal - 10;
+      }
+
+//      System.out.println(" "+sumVal+" latest value");
+      System.out.print(" "+sumVal);
+      
+      pastVal = latestVal;
+      latestVal = sumVal;
+//      System.out.println("sumVal    pastVal     latestVal     count     loop end");
+//      System.out.println(sumVal+" "+pastVal+" "+latestVal+" "+count+" loop end");
+//      System.out.println("firstNum : "+firstNum+" secNum : "+secNum);
+//      System.out.println("pastVal : "+pastVal+" latestVal : "+latestVal);
+      timeToEnd = (pastVal != firstNum && latestVal != secNum) ? false : true;
+//      System.out.println("timeToEnd : "+timeToEnd);
+      count++;
+      
+   }
+   System.out.println("");
+   System.out.println(count);
+  
+ br.close();
+ r.close();
+ }
+}
