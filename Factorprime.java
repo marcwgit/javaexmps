29e274abb92ba266063ea41953fdf9df5a0c62d7
marcwgit <marcwgit@gmail.com>
2016-12-10 00:01:19 -0500

    Update Factorprime.java

--- Factorprime.java
+++ Factorprime.java
@@ -20,7 +20,7 @@ public static void main(String args[])throws Exception{
 
 //   while (testnumber > 2){
      
-     for (int i=2; i< Math.sqrt(testnumber)+1; i++){
+     for (int i=2; i< Math.sqrt(testnumber)+2; i++){
 
 //      System.out.println("in 1 "+testnumber); 
       while (testnumber % i == 0){
