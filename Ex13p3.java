b4724a93f29414980e97e7a19e725c77755582ed
marcwgit <marcwgit@gmail.com>
2016-12-09 21:38:31 -0500

    Create Ex13p3.java

---
+++ Ex13p3.java
@@ -0,0 +1,6 @@
+import java.io.*;  
+public class Ex13p3{  
+public static void main(String args[])throws Exception{  
+  
+   InputStreamReader r=new InputStreamReader(System.in);  
+BufferedReader br=new BufferedReader(r); 
