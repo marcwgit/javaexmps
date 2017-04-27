25dd9a6a6aa3f204a1ba81dc7783fe960a7513f8
marcwgit <marcwgit@gmail.com>
2017-04-26 22:46:52 -0400

    Update IsPrime100c.java

--- IsPrime100c.java
+++ IsPrime100c.java
@@ -1,17 +1,31 @@
 //Demonstrates calling IsPrimec
 
-public class IsPrimec100c{
+
+
+public class IsPrime100c{
+
+
 
 	public static void main(String[] args)throws Exception{
+
 Integer testnumber;
+
 	IsPrimec testPrime = new IsPrimec();	
+
 		
+
 for(testnumber=1;testnumber<101; testnumber++){
 
-	testPrime.inputNumber = testnumber;
-	
-	testPrime.isPrime();
-		
-}	   
-}}
 
+
+	testPrime.inputNumber = testnumber;
+
+	
+
+	testPrime.isPrime();
+
+		
+
+}	   
+
+}}
