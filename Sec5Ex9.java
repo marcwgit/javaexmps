ae7e946ba0fb40620110e7c64b16615b11c90a00
marcwgit <marcwgit@gmail.com>
2017-05-02 12:36:52 -0400

    sec5ex9

--- Sec5Ex9.java
+++ Sec5Ex9.java
@@ -7,6 +7,7 @@ class Sec5Ex9{
    public static void main(String args[])
    {
 	   Integer mainMins, mainSecs;
+	   int[] HMSarray = new int[3];
 	mainMins = readInput("minutes");
 	mainSecs = readInput("seconds");
 	
@@ -17,6 +18,12 @@ class Sec5Ex9{
 	//System.out.println("in main "+readInput("seconds"));
 	
 	displayHMS(mainMins,mainSecs);
+	HMSarray = displayHMS(mainMins,mainSecs);
+	   System.out.println(HMSarray[2]);
+	   System.out.println(HMSarray[1]);
+	   System.out.println(HMSarray[0]);
+	   
+	
    }
    
    
@@ -40,20 +47,26 @@ class Sec5Ex9{
 	  return a;
    }
    
-   static void displayHMS(Integer inpMins, Integer inpSecs){
+   static int[] displayHMS(Integer inpMins, Integer inpSecs){
 	   
 	   Integer hours, mins, secs;
+	   int[] outHMSarray = new int[3];
 	   
 	   mins = inpSecs/60;
 	   secs = inpSecs - (mins*60);
+	   outHMSarray[0] = secs;
 	   
 	   inpMins = inpMins + mins;
 	   hours = inpMins/60;
+	   outHMSarray[2] = hours;
 	   
 	   mins = inpMins - (hours*60);
+	   outHMSarray[1] = mins;
 	   
 	   System.out.println(hours);
 	   System.out.println(mins);
 	   System.out.println(secs);
+	   return outHMSarray;
+	   
    }
 }
